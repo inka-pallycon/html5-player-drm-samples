e0489f4cf7cb39be54cebc2b756831c87003dd02
Brown Kwon <cancel83@inka.co.kr>
2021-09-30 09:52:11 +0900

    Update jwplayer-sample.js
    fixed fairplay error

--- js/jwplayer-sample.js
+++ js/jwplayer-sample.js
@@ -19,86 +19,40 @@ function configurePlayer() {
                         }]
                     }
                 }
-            }, {
+            }]
+        },{
+            "sources": [{
                 "file": hlsUri,
                 "drm": {
                     "fairplay": {
-                        "certificateUri": "https://license-global.pallycon.com/ri/fpsCert.do?siteId=DEMO", // fairplayCertUri
+                        "certificateUrl": "https://license-global.pallycon.com/ri/fpsCert.do?siteId=DEMO", // fairplayCertUri
                         "extractContentId": function (initDataUri) {
                             const contentId = initDataUri.substring(initDataUri.indexOf('skd://') + 6);
                             console.log('extractContentId - initDataUri: ' + initDataUri + ' contentId: ' + contentId);
                             return contentId;
-                            /*
-                            console.log(initDataUri);
-                            var uriParts = initDataUri.split('://', 2);
-                            var protocol = uriParts[0].slice(-3);
-                            console.log(protocol);
-                            var contentId = uriParts[1];
-                            return protocol.toLowerCase() == 'skd' ? contentId : '';
-                            */
                         },
-                        "processSpcUri": licenseUri,
-                        "licenseResponseType": "blob",
-                        "licenseRequestHeaders": [{
-                            "name": "Content-type",
-                            "value": "application/octet-stream"
-                        }],
-                        "licenseRequestMessage": function(message) {
-                            console.log('licenseRequestMessage');
-                            return new Blob([message], {
-                                type: 'application/octet-binary'
-                            });
-                        },
-                        "extractKey": function(response) {
-                            return new Promise(function(resolve, reject) {
-                                var reader = new FileReader();
-                                reader.addEventListener('loadend', function() {
-                                    resolve(new Uint8Array(reader.result));
-                                });
-                                reader.addEventListener('error', function() {
-                                    reject(reader.error);
-                                });
-                                reader.readAsArrayBuffer(response);
-                            });
-                        },                        
-                        /*
+                        "processSpcUrl": licenseUri,
                         "licenseResponseType": "text",
                         "licenseRequestHeaders": [{
                             "name": "Content-type",
                             "value": "application/x-www-form-urlencoded"
-                        }],
-                        "licenseRequestMessage": function(message, session) {
-                            console.log('licenseRequestMessage - contentId: '+ this.contentId);
-                            console.log('&assetId='+encodeURIComponent(this.contentId));
-                            return 'spc='+base64EncodeUint8Array(message)+'&assetId='+encodeURIComponent(this.contentId);
                         },
-                        "extractKey": function(ckc) {
-                            console.log('extractKey');
-                            var base64EncodedKey = ckc.trim();
-                            if (base64EncodedKey.substr(0, 5) === '' && base64EncodedKey.substr(-6) === '') {
-                                base64EncodedKey = base64EncodedKey.slice(5, -6);
+                            {
+                                "name": "pallycon-customdata-v2",
+                                "value": fairplayToken
                             }
-                            return base64EncodedKey;
+                        ],
+                        "licenseRequestMessage": function(message, session) {
+                            var payload = "spc=" + base64EncodeUint8Array(message);
+                            return payload;
                         },
-                        */                        
-                        "headers": [{
-                            "name": "pallycon-customdata-v2",
-                            "value": fairplayToken
-                        }]
+                        "extractKey": function(response) {
+                            console.log('license Response : ', response);
+                            return base64DecodeUint8Array(response);
+                        },
+
                     }
                 }
-            }]
-        }]
-    });
-}
-
-function configurePlayer2() {
-    jwplayer("myPlayer").setup({
-        "playlist": [{
-            "sources": [{
-                "file": "https://live.unified-streaming.com/scte35/scte35.isml/.mpd"
-            }, {
-                "file": hlsUri
             }]
         }]
     });
